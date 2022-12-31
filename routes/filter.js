b005cdd17b7aeda67b54231d838b13a8db02ccee
Thenuri <cb010529@students.apiit.lk>
2022-12-31 12:00:29 +0530

    changes

--- routes/filter.js
+++ routes/filter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-      
+
 router('/finder/:id',function(req,res,next){
     seatNo = req.body.seatNo
     price = req.body.price
@@ -10,7 +10,5 @@ router('/finder/:id',function(req,res,next){
     let select = "SELECT * FROM `bus` where `No_Of_Seats`= seatNo AND `AC_Status` = ac AND ORDER BY `Price_Per_Km` ="
 
 })
-
-
 
 
