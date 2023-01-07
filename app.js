2a304def561dee1bc5c102eeeeea8360922fd0dc
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-01-07 10:12:31 +0530

    Change to ejs

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ var app = express();
 
 
 // view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
