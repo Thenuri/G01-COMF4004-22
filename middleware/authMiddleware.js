cc5c6c4b5d812248ad1d42a32ee9ced4f70a672c
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-01-10 13:18:04 +0530

    Added redirects to auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,21 +4,23 @@ const accountController = require('../controllers/accountController')
 async function authenticateJWT (req, res, next) {
 
     if (req.cookies === undefined) {
-        return res.status(403).json({
-            error: {
-                message: "No Cookies Found"
-            }
-        })
+        return res.status(403).redirect('/signin?redirect=' + req.originalUrl);
+        // .json({
+        //     error: {
+        //         message: "No Cookies Found"
+        //     }
+        // })
     }
 
     const {token} = req.cookies
 
     if (token === undefined) {
-        return res.status(403).json({
-            error: {
-                message: "No Token Found"
-            }
-        })
+        return res.status(403).redirect('/signin?redirect=' + req.originalUrl);
+        // .json({
+        //     error: {
+        //         message: "No Token Found"
+        //     }
+        // })
     }
     let isValid
     // testtokenInvalid = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBY2NvdW50X0lEIjoyRW1haWwiOiJxYWFkc2Z3QHR0LmNvbSIsImlhdCI6MTY3MjE0ODA3MiwiZXhwIjoxNjcyMTQ4NjcyfQ.n9BtTYYuOelvtTyemSw73V8JIY7HZqMCka6u73bpGVY'
@@ -29,11 +31,7 @@ async function authenticateJWT (req, res, next) {
         // console.log(isValid)
     } catch (error) {
         console.log('authMiddleware error', error)
-        return res.status(403).json({
-            error: {
-                message: error.message
-            }
-        })
+        return res.status(403).redirect('/signin?redirect=' + req.originalUrl);
         
     }
 
@@ -51,7 +49,8 @@ async function authenticateJWT (req, res, next) {
     }
 
     if ( !isAccountActive) {
-        return res.status(403).json({ error:{ message : "Account not active / suspended"}})
+        return res.status(403).redirect('/signin?redirect=' + req.originalUrl);
+        // .json({ error:{ message : "Account not active / suspended"}})
     }
      
     let accountType;
