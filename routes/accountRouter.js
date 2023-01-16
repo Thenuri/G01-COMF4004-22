d4003c3dde4279506bb7cdcf80526a2fd96a6699
Thenuri <cb010529@students.apiit.lk>
2023-01-16 13:42:02 +0530

    changes

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -43,19 +43,19 @@ router.put("/profileUpdate", authenticateJWT, async function(req,res){
     const accountId = req.body.Account_ID
     const accountType =  req.body.AccountType
     const Name = req.body.Name;
-    const ContactNo = req.body.ContactNo;
+    const ContactNo = req.body.Contact_No;
     const Address = req.body.Address;
     let updates, values;
     if (accountType === "client"){
         client_ac = await clientController.findClientByAccountId(accountId);
-        console.log(client_ac)
         updates = "UPDATE `client` SET `Name` = ?, `Address` = ?, `Contact_No` = ? WHERE `client`.`Client_ID` = ?"
         values = [Name, Address, ContactNo, client_ac.Client_ID ]
     }else if (accountType === "owner"){
         owner_ac = await ownerController.findOwnerByAccountId(accountId);
         console.log(owner_ac)
-        updates = "UPDATE `bus_owner` SET `Contact_No` = ? , `Name` = ? , `Address` = ? WHERE `bus_owner`.`Owner_ID` = ?"
-        values = [Name, Address, ContactNo, owner_ac.owner_ID ]
+        updates = "UPDATE  `bus_owner` SET `Name` = ? , `Address` = ? , `Contact_No` = ? WHERE `bus_owner`.`Owner_ID` = ?"
+        values = [Name, Address, ContactNo, owner_ac.Owner_ID ]
+        console.log(values)
     } else {
         return res.json({error: {message: "Account type not valid"}})
     }
